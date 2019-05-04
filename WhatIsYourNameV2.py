4cd6bf683e946d3dd0136ff9c322795d209dbba2
smyousuf14 <44485548+smyousuf14@users.noreply.github.com>
2019-05-04 19:56:27 -0400

    Create WhatIsYourNameV2.py

---
+++ WhatIsYourNameV2.py
@@ -0,0 +1,15 @@
+# This program will read in the users name and gives an appropriate comment.
+
+name = None
+
+name = input("Enter your name: ");
+
+print("Your name is " + name);
+
+if(name == "Yousuf"):
+    print("Thats your last name.")
+elif(name == "Syed"):
+    print("Thats your first name.")
+else:
+    print("You are a normal man.")
+    
