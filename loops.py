40626aa417c163f8cb95ab8de5715b8ccafac142
smyousuf14 <44485548+smyousuf14@users.noreply.github.com>
2019-05-06 19:57:44 -0400

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,16 @@
+#This program will make use of all the loops avaliable in python.
+
+#Variable list
+counter = 0
+
+while(counter <= 8):
+    print(str(counter))
+    counter+=1
+
+for letter in "Python":
+    print("Letter: " + letter)
+
+fruits = ["banana", "apple", "pear"]
+
+for fruit in fruits:
+    print("The fruit is : " + fruit)
